988b7f8ed83158e84f77ac42c09ca32e9f99c89d
Monica Craveiro <craveiromonica@gmail.com>
2024-10-21 02:59:31 -0300

    Localization menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-  def set_locale
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+
     if params[:locale]
-      cookies[:locale] = params[:locale]
+      cookies[:locale] = locale
     end
     if cookies[:locale]
-      if I18n.locale != cookies[:locale]
+      if I18n.locale.to_s != cookies[:locale]
         I18n.locale = cookies[:locale]
       end
     end
+
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
